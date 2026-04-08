2ce002373131d416554eb7b014fd9e3bd0e75865
Mario <mabel010830@gmail.com>
2026-04-08 13:36:34 -0600

    deleted folder

--- src/components/pesca/data-table/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DataTable } from "./DataTable";
-export type { DataTableProps } from "./DataTable";
-export { default as SortableColumnHeader } from "./SortableColumnHeader";
--- src/types/barco.ts
+++
@@ -1,9 +0,0 @@
-export interface Barco {
-  id: number;
-  nombre: string;
-  identidad: string;
-  matricula: string;
-  tipo: string;
-  propietario: string;
-  puertoBase: string;
-}
